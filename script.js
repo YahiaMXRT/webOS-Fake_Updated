64c8472cac109cf8734ef0f22a88d0c8d404781e
YahiaMXRT <imnotapotato.632@gmail.com>
2025-07-22 19:53:59 +0300

    Fixing Xterm.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Handle dragging for all windows
 var backup = document.body.innerHTML;
-
+var term = new Terminal();
+term.open(document.getElementById('terminal'));
+term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
 document.querySelectorAll('.window').forEach((win) => {
   const titlebar = win.querySelector('.titlebar');
   let isDragging = false;
